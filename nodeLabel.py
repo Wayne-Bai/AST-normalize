e3c28420fc923eba08ce53d417e5a5ceb3a2e5b4
Wayne <bwh.buaa@gmail.com>
2020-08-31 15:13:27 -0400

    malware js code

---
+++ nodeLabel.py
@@ -0,0 +1,21 @@
+import json
+
+f = open('js2AST/AST.json', 'r')
+w = open('NodeLabel-AST.txt', 'a')
+
+node_label = []
+for line in f.readlines():
+    graphs_list = json.loads(line)
+    for dic in graphs_list:
+        if dic['type'] not in node_label:
+            node_label.append(dic['type'])
+
+print(len(node_label))
+print(node_label)
+
+for i in range(len(node_label)):
+    w.write(node_label[i] + ' ' + str(i+1))
+    w.write('\n')
+
+f.close()
+w.close()
